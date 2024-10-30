1891520c0ad7db37b1b6cbc02caac649caf4e25f
Erfan Ansari <dev.erfanansari@gmail.com>
2024-10-30 22:29:36 +0330

    feat: add first problem

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  proseWrap: 'always',
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+}
